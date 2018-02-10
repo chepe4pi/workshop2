8f6011b4c8bce1d53dc7396507e943280b2f05ec
Alexey Kurylev <chepe4pi@gmail.com>
2018-02-10 19:12:18 +0300

    add render view

--- audiolibary/views.py
+++ audiolibary/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+from audiolibary.models import Song
+
+
+def get_songs(request):
+    songs = Song.objects.all()
+    return render_to_response('index.html', context={'songs': songs})
+
--- workshop2/urls.py
+++ workshop2/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from audiolibary.views import get_songs
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_songs)
 ]
